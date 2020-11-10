efc9b1cda84e45ad73e57ffd9b8b67937c05d331
Rus Chr <r.chergizbiev@gmail.com>
2020-11-10 19:02:42 +0300

    Initial Commit

---
+++ GitHub_Followers_take2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub_Followers_take2
+//
+//  Created by Admin on 10.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
