9f119f0e95814c482440fb992a22d31359327599
Rus Chr <r.chergizbiev@gmail.com>
2020-11-17 16:33:13 +0300

    04: done

---
+++ GitHub_Followers_take2/FavoritesVC.swift
@@ -0,0 +1,28 @@
+//
+//  FavoritesVC.swift
+//  GitHub_Followers_take2
+//
+//  Created by Admin on 17.11.2020.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GitHub_Followers_take2/SearchVC.swift
@@ -0,0 +1,28 @@
+//
+//  SearchVC.swift
+//  GitHub_Followers_take2
+//
+//  Created by Admin on 17.11.2020.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
