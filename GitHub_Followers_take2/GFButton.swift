8e41016e8965ca9213e4de62d368b01d48a3f835
Rus Chr <r.chergizbiev@gmail.com>
2020-11-17 17:25:20 +0300

    05: started

---
+++ GitHub_Followers_take2/GFButton.swift
@@ -0,0 +1,37 @@
+//
+//  GFButton.swift
+//  GitHub_Followers_take2
+//
+//  Created by Admin on 17.11.2020.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
