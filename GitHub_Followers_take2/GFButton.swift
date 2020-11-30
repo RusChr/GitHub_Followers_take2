c123a828e64b7b64481bcd193be6a391dd989913
Rus Chr <r.chergizbiev@gmail.com>
2020-11-30 19:25:04 +0300

    05: done

--- GitHub_Followers_take2/GFButton.swift
+++ GitHub_Followers_take2/GFButton.swift
@@ -11,6 +11,7 @@ class GFButton: UIButton {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
     }
     
     
@@ -19,10 +20,19 @@ class GFButton: UIButton {
     }
     
     
+    init(backgoundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
     private func configure() {
         layer.cornerRadius = 10
         titleLabel?.textColor = .white
         titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
     }
     
 
