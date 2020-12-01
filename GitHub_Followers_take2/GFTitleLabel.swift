e8592339f304a29bf3f19e7ecf40a35a70277940
Rus Chr <r.chergizbiev@gmail.com>
2020-12-01 19:20:38 +0300

    08: start

---
+++ GitHub_Followers_take2/GFTitleLabel.swift
@@ -0,0 +1,48 @@
+//
+//  GFTitleLabel.swift
+//  GitHub_Followers_take2
+//
+//  Created by Admin on 01.12.2020.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
