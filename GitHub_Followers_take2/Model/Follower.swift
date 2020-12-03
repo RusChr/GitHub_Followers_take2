02257ca25dfd36c2d10dc4d713fd9bb9b3da6ea3
Rus Chr <r.chergizbiev@gmail.com>
2020-12-03 19:11:42 +0300

    10: done

---
+++ GitHub_Followers_take2/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHub_Followers_take2
+//
+//  Created by Admin on 03.12.2020.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GitHub_Followers_take2/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GitHub_Followers_take2
+//
+//  Created by Admin on 03.12.2020.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
