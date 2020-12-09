0fd644b19623df97bf954b4d17e20a1863b4bffc
Rus Chr <r.chergizbiev@gmail.com>
2020-12-09 15:59:24 +0300

    28: done

--- GitHub_Followers_take2/Screens/UserInfoVC.swift
+++ GitHub_Followers_take2/Screens/UserInfoVC.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class UserInfoVC: UIViewController {
     
+    let headerView = UIView()
+    
     var username: String!
     
 
@@ -19,12 +21,16 @@ class UserInfoVC: UIViewController {
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
         
+        layoutUI()
+        
         NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
             guard let self = self else { return }
             
             switch result {
             case .success(let user):
-                print(user)
+                DispatchQueue.main.async {
+                    self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                }
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
             }
@@ -34,5 +40,26 @@ class UserInfoVC: UIViewController {
     
     @objc private func dismissVC() {
         dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
+    }
+    
+    
+    private func layoutUI() {
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(greaterThanOrEqualTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
     }
 }
---
+++ GitHub_Followers_take2/Utilities/Constans.swift
@@ -0,0 +1,12 @@
+//
+//  Constans.swift
+//  GitHub_Followers_take2
+//
+//  Created by Admin on 09.12.2020.
+//
+
+import Foundation
+
+enum SFSybmols {
+    static let location = "mappin.and.ellipse"
+}
