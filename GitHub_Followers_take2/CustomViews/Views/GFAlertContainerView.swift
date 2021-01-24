5de94f0ee905e64381eef9292fcab86575a23118
Rus Chr <r.chergizbiev@gmail.com>
2021-01-24 20:54:29 +0300

    41: start

---
+++ GitHub_Followers_take2/CustomViews/Views/GFAlertContainerView.swift
@@ -0,0 +1,31 @@
+//
+//  GFAlertContainerView.swift
+//  GitHub_Followers_take2
+//
+//  Created by Rustam Chergizbiev on 24.01.2021.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
